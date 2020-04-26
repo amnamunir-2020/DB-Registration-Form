8337b00961b7957c92f6691cc3e7508164702849
amnamunir-2020 <59399151+amnamunir-2020@users.noreply.github.com>
2020-04-27 01:34:38 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace User_Registration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.AutoSize = true;
+            this.txtName.Location = new System.Drawing.Point(78, 94);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(41, 13);
+            this.txtName.TabIndex = 0;
+            this.txtName.Text = "label11";
+            this.txtName.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(498, 428);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label txtName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace User_Registration
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source=DESKTOP-GJF4KSU; Initial Catalog=UserRegistrationDB; Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlcon= new SqlConnection(connectionString)) 
+            sqlcon.Open();
+            SqlCommand sqlcmd = new SqlCommand("UserAdd,sqlcon");
+            sqlcmd.CommandType = CommandType.StoredProcedure;
+            sqlcmd.Parameters.AddWithValue("@FirstName",FirstName.Text.Trim);
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
